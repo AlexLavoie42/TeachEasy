0cc5db274b529bbd25e0684c38b03731676f285f
Alex Lavoie <alexflavoie@gmail.com>
2019-04-30 15:21:28 -0700

    Very basic math text editor prototype. Mashed MathLive + Quill

--- TeachEasy/App_Start/BundleConfig.cs
+++ TeachEasy/App_Start/BundleConfig.cs
@@ -29,6 +29,9 @@ namespace TeachEasy
                 "~/Scripts/app/home.viewmodel.js",
                 "~/Scripts/app/_run.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/text").Include(
+                "~/Scripts/app/text.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
---
+++ TeachEasy/Scripts/app/text.js
@@ -0,0 +1,28 @@
+﻿var quill = new Quill('#editor', {
+    theme: 'snow',
+    modules: {
+        toolbar: '#toolbar'
+    }
+});
+
+let BlockEmbed = Quill.import('blots/block/embed');
+
+class MathBlot extends BlockEmbed {
+    static create() {
+        let node = super.create();
+        MathLive.makeMathField(node);
+        return node;
+    }
+}
+MathBlot.blotName = 'math';
+MathBlot.tagName = 'span';
+
+Quill.register(MathBlot);
+
+var customButton = document.querySelector('#custom-button');
+customButton.addEventListener('click', function () {
+    let range = quill.getSelection(true);
+    quill.insertText(range.index, '\n', Quill.sources.USER);
+    quill.insertEmbed(range.index + 1, 'math', true, Quill.sources.USER);
+    quill.setSelection(range.index + 2, Quill.sources.SILENT);
+});
