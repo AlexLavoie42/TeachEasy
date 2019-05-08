75bcf27b617c02bf456f2303c4f2fcf782f640c2
Sa-eedA <sa-eed_abdi@hotmail.com>
2019-05-07 19:39:21 -0700

    second attempt at committing prior 3 commits

--- TeachEasy/App_Start/BundleConfig.cs
+++ TeachEasy/App_Start/BundleConfig.cs
@@ -11,7 +11,8 @@ namespace TeachEasy
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
@@ -35,11 +36,15 @@ namespace TeachEasy
                 "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/Site.css",
                  "~/Content/bootstrap.css",
-                 "~/Content/Site.css"));
+                 "~/Content/bootstrap.min.css",
+                 "~/Content/simple-sidebar.css")
+                 );
         }
     }
 }
--- TeachEasy/Controllers/HomeController.cs
+++ TeachEasy/Controllers/HomeController.cs
@@ -5,11 +5,11 @@ using System.Web.Mvc;
 
 namespace TeachEasy.Controllers
 {
-    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+       
             return View();
         }
     }
