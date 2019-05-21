ef16743601894c2b9fd67274195ea8ab19e01ecf
AlexLavoie <lavoiealex@hotmail.com>
2019-05-21 14:49:28 -0700

    Bugs

--- TeachEasy/Migrations/Configuration.cs
+++ TeachEasy/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace TeachEasy.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(TeachEasy.Models.ApplicationDbContext context)
